3294ec3d483e13fca38149bb1c6ee0c80f8fd511
Marko Stojiljkovic <stojiljkovicmarko@yahoo.com>
2021-09-02 19:19:43 +0200

    done with local storage for users

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -13,45 +13,13 @@ const UserCard = (props) => {
 
     const red = utils.isFemale(gender) ? "red" : "";
 
-    const hideEmail = (email) => {
-        let [beforeMonkey, afterMonkey] = email.split("@");
-        let firstPartBeforeMonkey = beforeMonkey.slice(0, 3) + "...";
-        let secondPartBeforeMonkey = beforeMonkey.slice(beforeMonkey.length - 2, beforeMonkey.lenght);
-        return (firstPartBeforeMonkey + secondPartBeforeMonkey + "@" + afterMonkey);
-    }
-
-    const ordinalNumber = (day) => {
-        switch (day) {
-            case 1:
-                return day + "st";
-            case 2:
-                return day + "nd";
-            case 3:
-                return day + "rd";
-            default:
-                return day + "th";
-        }
-
-    }
-
-    const formatDate = (date) => {
-        let dateObj = new Date(date);
-        const monthNames = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ];
-        let day = dateObj.getDate();
-        let month = dateObj.getMonth();
-        let year = dateObj.getYear();
-        return (monthNames[month] + " " + ordinalNumber(day) + " " + year);
-    }
-
     return (
         <div className={`card ${red} col-4 pt-2 mb-2 `}>
             <img className="card-img" src={large} alt="" />
             <div className="card-body">
                 <p className="card-text">Name: {first}</p>
-                <p>Email: {hideEmail(email)} </p>
-                <p>Date: {formatDate(date)}</p>
+                <p>Email: {utils.hideEmail(email)} </p>
+                <p>Date: {utils.formatDate(date)}</p>
             </div>
         </div>
     );
